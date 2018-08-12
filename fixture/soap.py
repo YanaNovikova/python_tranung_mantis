ce71bb838beaa879be8527e5593b6d4d467aaf2b
Yana Novikova <Zyf.15@mail.ru>
2018-08-12 15:15:29 +0300

    soap

---
+++ fixture/soap.py
@@ -0,0 +1,15 @@
+from suds.client import Client
+from suds import WebFault
+
+5class SoapHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def can_login(self, username, password):
+        client = Client("http://localhost:8080/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        try:
+            client.servise.mc_login(username, password)
+            return True
+        except WebFault:
+            return False
